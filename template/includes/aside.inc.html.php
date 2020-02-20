f4903daac54b55de51376d3debbfc862f1a9ae23
mroslogos <61208811+mroslogos@users.noreply.github.com>
2020-02-19 18:18:53 -0800

    Add files via upload

---
+++ template/includes/aside.inc.html.php
@@ -0,0 +1,11 @@
+<aside>
+            <h2>Heading 2 (H2)</h2>
+            <div>
+                <h3>Aside 1 | Heading 3 (H3)</h3>
+                <ul>
+                    <li></li>
+                    <li></li>
+                </ul>
+
+            </div>
+        </aside>
--- template/index.php
+++ template/index.php
@@ -18,101 +18,10 @@
 <body>
     <?php include 'includes/header.inc.html.php' ?>
     <main>
-        <section>
-            <h2>Heading 2 (H2)</h2>
-            <div>
-                <h3>Section 1 | Heading 3 (H3)</h3>
-                <ul>
-                    <li>List Thing 1</li>
-                    <li>List Thing 2</li>
-                    <li>List Thing 3</li>
-                    <li>List Thing 4</li>
-                    <li>List Thing 5</li>
-                </ul>
-                <p>Annoy kitten brother with poking. A nice warm laptop for me to sit on white cat sleeps on a black
-                    shirt so have my breakfast spaghetti yarn, or cat snacks, and plan your travel. Bathe private parts
-                    with tongue then lick owner's face. Scream at teh bath sit by the fire, i'm going to lap some water
-                    out of my master's cup meow litter box is life. Rub face on everything fall over dead (not really
-                    but gets sypathy) run at 3am, going to catch the red dot today going to catch the red dot today if
-                    human is on laptop sit on the keyboard but use lap as chair, Pet me pet me pet me pet me, bite,
-                    scratch, why are you petting
-                    me?</p>
-                <p>Making bread on the bathrobe russian blue but push your water glass on the floor so i bet my nine
-                    lives on you-oooo-ooo-hooo, adventure always yet bring your owner a dead bird making sure that fluff
-                    gets into the owner's eyes. Found somthing move i bite it tail curl up and sleep on the freshly
-                    laundered towels. Sit in window and stare oooh, a bird, yum russian blue what a cat-ass-trophy! but
-                    chill on the couch table. I like cats because they are fat and fluffy. Meowzer has closed eyes but
-                    still sees you kitty pounce, trip, faceplant you didn't see that no you didn't definitely didn't
-                    lick, lick, lick, and preen away the embarrassment for kitty scratches couch bad kitty.</p>
-            </div>
-        </section>
-        <section>
-            <h2>Heading 2 (H2)</h2>
-            <div>
-                <h3>Section 2 | Heading 3 (H3)</h3>
-                <ul>
-                    <li>List Thing 1</li>
-                    <li>List Thing 2</li>
-                    <li>List Thing 3</li>
-                    <li>List Thing 4</li>
-                    <li>List Thing 5</li>
-                </ul>
-                <p>Annoy kitten brother with poking. A nice warm laptop for me to sit on white cat sleeps on a black
-                    shirt so have my breakfast spaghetti yarn, or cat snacks, and plan your travel. Bathe private parts
-                    with tongue then lick owner's face. Scream at teh bath sit by the fire, i'm going to lap some water
-                    out of my master's cup meow litter box is life. Rub face on everything fall over dead (not really
-                    but gets sypathy) run at 3am, going to catch the red dot today going to catch the red dot today if
-                    human is on laptop sit on the keyboard but use lap as chair, Pet me pet me pet me pet me, bite,
-                    scratch, why are you petting
-                    me?</p>
-                <p>Making bread on the bathrobe russian blue but push your water glass on the floor so i bet my nine
-                    lives on you-oooo-ooo-hooo, adventure always yet bring your owner a dead bird making sure that fluff
-                    gets into the owner's eyes. Found somthing move i bite it tail curl up and sleep on the freshly
-                    laundered towels. Sit in window and stare oooh, a bird, yum russian blue what a cat-ass-trophy! but
-                    chill on the couch table. I like cats because they are fat and fluffy. Meowzer has closed eyes but
-                    still sees you kitty pounce, trip, faceplant you didn't see that no you didn't definitely didn't
-                    lick, lick, lick, and preen away the embarrassment for kitty scratches couch bad kitty.</p>
-            </div>
-        </section>
-        <section>
-            <h2>Heading 2 (H2)</h2>
-            <div>
-                <h3>Section 3 | Heading 3 (H3)</h3>
-                <ul>
-                    <li>List Thing 1</li>
-                    <li>List Thing 2</li>
-                    <li>List Thing 3</li>
-                    <li>List Thing 4</li>
-                    <li>List Thing 5</li>
-                </ul>
-                <p>Annoy kitten brother with poking. A nice warm laptop for me to sit on white cat sleeps on a black
-                    shirt so have my breakfast spaghetti yarn, or cat snacks, and plan your travel. Bathe private parts
-                    with tongue then lick owner's face. Scream at teh bath sit by the fire, i'm going to lap some water
-                    out of my master's cup meow litter box is life. Rub face on everything fall over dead (not really
-                    but gets sypathy) run at 3am, going to catch the red dot today going to catch the red dot today if
-                    human is on laptop sit on the keyboard but use lap as chair, Pet me pet me pet me pet me, bite,
-                    scratch, why are you petting
-                    me?</p>
-                <p>Making bread on the bathrobe russian blue but push your water glass on the floor so i bet my nine
-                    lives on you-oooo-ooo-hooo, adventure always yet bring your owner a dead bird making sure that fluff
-                    gets into the owner's eyes. Found somthing move i bite it tail curl up and sleep on the freshly
-                    laundered towels. Sit in window and stare oooh, a bird, yum russian blue what a cat-ass-trophy! but
-                    chill on the couch table. I like cats because they are fat and fluffy. Meowzer has closed eyes but
-                    still sees you kitty pounce, trip, faceplant you didn't see that no you didn't definitely didn't
-                    lick, lick, lick, and preen away the embarrassment for kitty scratches couch bad kitty.</p>
-            </div>
-        </section>
-        <aside>
-            <h2>Heading 2 (H2)</h2>
-            <div>
-                <h3>Aside 1 | Heading 3 (H3)</h3>
-                <ul>
-                    <li></li>
-                    <li></li>
-                </ul>
-
-            </div>
-        </aside>
+        <?php include 'includes/section.inc.html.php' ?>
+        <?php include 'includes/section.inc.html.php' ?>
+        <?php include 'includes/section.inc.html.php' ?>
+        <?php include 'includes/aside.inc.html.php' ?>
     </main>
     <?php include 'includes/footer.inc.html.php' ?>
 </body>
