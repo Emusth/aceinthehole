b4a3238b44d1b2e3027814a3b5ca0233e5d586b1
mroslogos <61208811+mroslogos@users.noreply.github.com>
2020-02-19 17:48:20 -0800

    Add files via upload

---
+++ template/includes/footer.inc.html.php
@@ -0,0 +1,4 @@
+<footer>
+        <p>Copyright &copy;2020 Ace in the Hole Multisport Events</p>
+    </footer>
+<script src="js/script.js"></script>
---
+++ template/includes/header.inc.html.php
@@ -0,0 +1,17 @@
+<?php define('BASE_URL', '/cas222/template/') ?>
+<header>
+        <div>
+            <h1>Heading 1 (H1)<span>Background hero image</span></h1>
+        </div>
+        <nav>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fas fa-bars fa-2x"></i>
+            </a>
+            <div id="myLinks">
+                <a href="#" class="active">Menu Item 1</a>
+                <a href="#">Menu Item 2</a>
+                <a href="#">Menu Item 3</a>
+                <a href="#">Menu Item 4</a>
+            </div>
+        </nav>
+    </header>
